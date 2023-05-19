1934713f7f63425f41e96d612c3ac53fd5fb17f0
haniyeh mirzapour <m.haniyeh.76@gmail.com>
2023-05-19 22:43:55 +0330

    get news list

---
+++ composables/useNews.js
@@ -0,0 +1,20 @@
+export const useNewsList = () => {
+    const { data, pending, error, execute } = useAsyncData("news", () =>
+      $fetch(`News/NewsByDate`, {
+        method: "GET",
+        baseURL: "http://newspodium.sakku-khatam.ir",
+        params: {
+          NewsAgencyId: 20,
+          Page: 1,
+          date: '2023 05 18'
+        },
+      })
+    );
+    onMounted(() => {
+        if (!unref(data)) {
+            execute();
+        }
+    })
+    let news = data._rawValue
+    return { news, pending }
+}
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div v-if="news" v-for="(item, index) in news.collection" :key="index"> 
+      <nuxt-link
+        :to="'/news/' + item.titlePage"
+        class="cursor-pointer block"
+      >
+        {{ item.titlePage }}
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useNewsList } from "~/composables/useNews";
+const { news, pending } = useNewsList();
+
+// const { data: news, error } = await useAsyncData("news", () =>
+//   $fetch(`News/NewsByDate`, {
+//     method: "GET",
+//     baseURL: "http://newspodium.sakku-khatam.ir",
+//     params: {
+//       NewsAgencyId: 20,
+//       Page: 1,
+//       date: '2023 05 18'
+//     },
+//   })
+// );
+</script>
+<style scoped lang="postcss">
+.g-head-anime {
+  @apply invisible;
+}
+</style>
