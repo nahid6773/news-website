de6ccc082c77d2e129b2a1fd9793dcf1fd79fe35
haniyeh mirzapour <m.haniyeh.76@gmail.com>
2023-05-28 23:36:53 +0330

    base Avatar component

---
+++ components/Base/Avatar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-20 h-20 mr-2 float-right block overflow-hidden relative align-middle m-auto rounded-full">
+    <img
+    class="w-full h-full object-cover border-4 border-gray-400 rounded-full hover:border-primary cursor-pointer"
+      src="https://picsum.photos/100/100"
+      alt=""
+    />
+  </div>
+</template>
