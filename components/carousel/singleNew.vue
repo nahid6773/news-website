18b1cc01a1831e453434f7911dee9f3d21871e9c
haniyeh mirzapour <m.haniyeh.76@gmail.com>
2023-05-30 22:23:02 +0330

    slider button

--- components/carousel/singleNew.vue
+++ components/carousel/singleNew.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <div class="just-desktop py-4">
+    <div class="relative py-4">
       <swiper
         class=""
         :slides-per-view="1"
@@ -22,23 +22,23 @@
         :modules="modules"
       >
         <swiper-slide v-for="(item, index) in items" :key="index">
-          <BaseHorizontalCard :info="item" class="my-4">
-            <template #footer>
-              <div class="flex mt-12 space-x-4 justify-end">
-                <button
-                  class="carousel1-next-single-card border-2 border-gray-200 text-gray-400 hover:border-gray-400 hover:text-gray-700 w-7 h-7 flex items-center justify-center mx-1 px-1"
-                >
-                  <IconArrowLeft class="rotate-180" />
-                </button>
-                <button
-                  class="carousel1-prev-single-card border-2 border-gray-200 text-gray-400 hover:border-gray-400 hover:text-gray-700 w-7 h-7 flex items-center justify-center mx-1 px-1"
-                >
-                  <IconArrowLeft />
-                </button>
-              </div>
-            </template>
-          </BaseHorizontalCard>
+          <BaseHorizontalCard :info="item" class="my-4" />
         </swiper-slide>
+        <div
+          class="absolute left-0 flex -mt-16 space-x-4 justify-end"
+          style="z-index: 999"
+        >
+          <button
+            class="carousel1-next-single-card border-2 border-gray-200 text-gray-400 hover:border-gray-400 hover:text-gray-700 w-7 h-7 flex items-center justify-center mx-1 px-1"
+          >
+            <IconArrowLeft class="rotate-180" />
+          </button>
+          <button
+            class="carousel1-prev-single-card border-2 border-gray-200 text-gray-400 hover:border-gray-400 hover:text-gray-700 w-7 h-7 flex items-center justify-center mx-1 px-1"
+          >
+            <IconArrowLeft />
+          </button>
+        </div>
       </swiper>
     </div>
   </section>
