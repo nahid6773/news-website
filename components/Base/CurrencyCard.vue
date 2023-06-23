987fb09ef5ee15bf859bf39b27cc323f8affb8a0
haniyeh mirzapour <m.haniyeh.76@gmail.com>
2023-06-23 22:13:56 +0330

    fix count of currency

--- components/Base/CurrencyCard.vue
+++ components/Base/CurrencyCard.vue
@@ -1,19 +1,19 @@
 <template>
-    <NuxtLink to="/currency" v-for="item in data" :key="item.currencyType">
+    <NuxtLink to="/currency" v-for="n in 7" :key="n">
       <div
         class="bg-white w-40 flex flex-col items-center rounded cursor-pointer"
       >
         <div
           class="w-full flex items-center justify-center bg-gray-200 hover:bg-gray-300 text-center py-2 text-sm font-bold"
         >
-          <IconChevronUp class="ml-1" />{{ item.currencyName }}
+          <IconChevronUp class="ml-1" />{{ data[n].currencyName }}
         </div>
         <div
           class="px-3 py-2 w-full flex flex-col items-center justify-center gap-y-2"
         >
-          <span class="font-bold text-greenlight">{{ item.max }}</span>
+          <span class="font-bold text-greenlight">{{ data[n].max }}</span>
           <hr class="w-full" />
-          <span>{{ item.change }}</span>
+          <span>{{ data[n].change }}</span>
         </div>
       </div>
     </NuxtLink>
