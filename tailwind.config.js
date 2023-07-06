05a0c41498bd5e23598e42e023a7c11e7ddbddcf
haniyeh mirzapour <m.haniyeh.76@gmail.com>
2023-07-07 00:29:38 +0330

    setting FAB

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require('tailwindcss/colors')
-const custom_colors = require('./style/theme.js')
+
+// const fontSize = var(--font-size)
+// const fontSize_sm = `${fontSize - 2 }px`
+// const fontSize_base = `${fontSize }px`
+// const fontSize_lg = `${fontSize + 2 }px`
 
 module.exports = {
   darkMode: 'class',
@@ -42,6 +46,22 @@ module.exports = {
       'laptop-m': '1250px',
       'laptop-l': '1440px',
       '4k': '2560px',
+    },
+    fontSize: {
+      xxs: ['0.675rem', { lineHeight: '.875rem' }],
+      xs: ['0.75rem', { lineHeight: '1rem' }],
+      sm: "var(--font-size)-2px",
+      base: "var(--font-size)-2px",
+      lg: "var(--font-size)-2px",
+      xl: ['1.25rem', { lineHeight: '1.75rem' }],
+      '2xl': ['1.5rem', { lineHeight: '2rem' }],
+      '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+      '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
+      '5xl': ['3rem', { lineHeight: '1' }],
+      '6xl': ['3.75rem', { lineHeight: '1' }],
+      '7xl': ['4.5rem', { lineHeight: '1' }],
+      '8xl': ['6rem', { lineHeight: '1' }],
+      '9xl': ['8rem', { lineHeight: '1' }], 
     },
     extend: {
       animation: {
